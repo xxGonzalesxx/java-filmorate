de73634743c6a74b069fccfa9900cd0f58cf8e13
Mr.Gonzales <151160699+xxGonzalesxx@users.noreply.github.com>
2025-12-02 19:59:56 +0400

    fix: исправить checkstyle ошибки - добавить пустые строки между методами

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User user);
+
     List<User> findAll();
+
     Optional<User> findById(Long id);
+
     boolean existsById(Long id);
 }
